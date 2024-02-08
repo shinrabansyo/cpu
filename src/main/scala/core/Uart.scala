83c76aac7c7c0908aeefc667c3c1f038fab2feac
wakuto <waku.902002@gmail.com>
2024-02-08 23:58:32 +0900

    UartRxの実装
    Co-authored-by: Kyuta <Ky00Ge@users.noreply.github.com>
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>

--- src/main/scala/core/Uart.scala
+++ src/main/scala/core/Uart.scala
@@ -1,3 +1,8 @@
+///////////////////////////////////////////////////////////////////////////////
+// セキュリティキャンプ 資料 を参照 (2024/2/8)
+// https://github.com/ciniml/seccamp_riscv_cpu/blob/2023/doc/slide/slide_1.pdf
+///////////////////////////////////////////////////////////////////////////////
+
 package core
 
 import chisel3._
@@ -6,7 +11,7 @@ import chisel3.util._
 class UartTx(clockFrequency: Int, baudRate: Int) extends Module {
   val io = IO(new Bundle {
     val tx       = Output(Bool())
-    val din      = Decoupled(UInt(8.W))  // 任意のデータを送るとき
+    val din      = Flipped(Decoupled(UInt(8.W)))  // 任意のデータを送るとき
   })
   // din.ready, din.valid, din.bits が使用可能
   // 送信側：din.valid をアサートすると din.bits が読み取り可能
@@ -37,9 +42,9 @@ class UartTx(clockFrequency: Int, baudRate: Int) extends Module {
   }
   when( bitCounter > 0.U ) {
     when(rateCounter === 0.U) { // 次のボーレート周期の送信タイミング
-      bits := Cat(bits(9), bits(9, 1))    // 全体を一つ下の桁にずらしただけ(最上位bitは保持)
+      // bits := Cat(bits(9), bits(9, 1))    // 全体を一つ下の桁にずらしただけ(最上位bitは保持)
       // bits := (bits.S >> 1.U).U
-      // (0 to 8).foreach(i => bits(i) := bits(i + 1)) // ビットバッファを右シフトする
+      (0 to 8).foreach(i => bits(i) := bits(i + 1)) // ビットバッファを右シフトする
       bitCounter := bitCounter - 1.U
       rateCounter := (baudDivider - 1).U
     } .otherwise {
@@ -48,45 +53,74 @@ class UartTx(clockFrequency: Int, baudRate: Int) extends Module {
   }
 }
 
-// val uart9600bps = Module(new UartTx(10_000_000, 9600))
-// val uart115200bps = Module(new UartTx(10_000_000, 115200))
-// std::vector<int> v = {1, 99, 4};
-// std::vector<string> s = {"a", "b", "c"};
-///////////////////////////////////////////////////////////////////////////////
-// セキュリティキャンプ 資料より引用
-// https://github.com/ciniml/seccamp_riscv_cpu/blob/2023/doc/slide/slide_1.pdf
-///////////////////////////////////////////////////////////////////////////////
-// class UartTx(clockFrequency: Int, baudRate: Int) extends Module {
-//   val io = IO(new Bundle{
-//   val tx = Output(Bool())
-//   })
-//   val baudDivider = clockFrequency/baudRate // クロック周波数/ボー‧レート
-//   val rateCounter = RegInit(0.U(log2Ceil(baudDivider).W)) // ボー‧レート周期⽣成⽤カウンタ
-//   val bitCounter = RegInit(0.U(log2Ceil(8 + 2).W)) // 残り送信ビット数カウンタ
-//   val bits = Reg(Vec(8 + 2, Bool())) // 送信ビット‧バッファ
-// 
-//   io.tx := bitCounter === 0.U || bits(0) // 送信中ならbit0を出⼒。それ以外は'1'を出⼒
-//   val ready = bitCounter === 0.U // ビット‧カウンタ == 0なので、次の送信を開始できるか？
-//   when(ready) {
-//     bits := Cat(1.U, 0x41.U(8.W), 0.U).asBools // STOP(1), 'A', START(0)
-//     bitCounter := (8 + 2).U // 残送信ビット数 = 10bit (STOP + DATA + START)
-//     rateCounter := (baudDivider - 1).U // レートカウンタを初期化
-//   }
-//   when( bitCounter > 0.U ) {
-//     when(rateCounter === 0.U) { // 次のボーレート周期の送信タイミング
-//       (0 to 8).foreach(i => bits(i) := bits(i + 1)) // ビットバッファを右シフトする
-//       bitCounter := bitCounter - 1.U
-//       rateCounter := (baudDivider - 1).U
-//     } .otherwise {
-//       rateCounter := rateCounter - 1.U
-//     }
-//   }
-// }
-///////////////////////////////////////////////////////////////////////////////
-
-class UartRx extends Module {
-  val io = IO(new Bundle {
-    val rx       = Input(Bool())
-    val dout     = Decoupled(UInt(8.W))
+class UartRx(clockFrequency: Int, baudRate: Int, rxSyncStages: Int) extends Module {
+  val io = IO(new Bundle{
+    val out = Decoupled(UInt(8.W)) // 受信データを出⼒
+    val rx = Input(Bool())         // UART信号⼊⼒
+    val overrun = Output(Bool())   // UARTデータ取りこぼし発⽣？
   })
+  val baudDivider = clockFrequency/baudRate               // クロック周波数÷ボー‧レート
+  val rateCounter = RegInit(0.U(log2Ceil(baudDivider*3/2).W))
+  // P.103 a～c を測るため 1.5倍
+  val bitCounter = RegInit(0.U(log2Ceil(8 + 2).W))
+  val bits = Reg(Vec(8 + 2, Bool()))
+  // UARTから受け取るデータを入れる箱
+  val rxRegs = RegInit(VecInit((0 to rxSyncStages + 1 - 1).map(_ => false.B)))
+  // rxSyncStages : 同期に使うフリップフロップ回路の個数
+  // if(0 to rxSyncStages + 1 - 1) <=> if(0 < rxSyncStages + 1)
+  // [0, 1, ... ,rxSyncStages] を生成して [false, false, ... ,false] にしている
+  val overrun = RegInit(false.B)        // CPU側に送信中にUART相手から受信させられそうな時のフラグ
+  val running = RegInit(false.B)        // 受信中のフラグ
+
+  // 受信データの出⼒信号 (VALID/READYハンドシェーク)
+  val outValid = RegInit(false.B)            // UART相手からの受信完了で立つ
+  val outBits = Reg(UInt(8.W))    // CPUに送信するデータ
+  val outReady = WireDefault(io.out.ready)   // CPU側から読み取られたら(送信完了で)立つ
+  io.out.valid := outValid
+  io.out.bits := outBits
+
+  // やるべきタスクが終わったら
+  when(outValid && outReady) {
+    outValid := false.B // VALID&READY成⽴したのでVALIDを落とす
+  }
+
+  // RX信号をクロックに同期
+  rxRegs(rxSyncStages) := io.rx
+  (0 to rxSyncStages - 1).foreach(i => rxRegs(i) := rxRegs(i + 1))
+  // 相手は相手自身のクロックに合わせて送信してくるけど、自分側のクロックとそのクロックは
+  // 同期していないので合わせる必要がある
+  // UARTからの入力　=> [ rxRegs ] => 受信
+  //                       ↑ こいつを挟んで同期してる
+
+  io.overrun := overrun
+
+  // 受信中じゃない時
+  when(!running) {
+    when(!rxRegs(1) && rxRegs(0)) {
+      // スタートビット検出(立ち下がり検出)、rxRegs(0)が1(平常電位)、rxRegs(1)がゼロ(start信号)
+      rateCounter := (baudDivider * 3 / 2 - 1).U // Wait until the center of LSB.
+      bitCounter := (8 + 2 - 1).U
+      running := true.B
+    }
+  }.otherwise {
+    when(rateCounter === 0.U) { // 1ビット周期ごとに処理
+      bits(8 + 2 -1) := rxRegs(0) // つぎのビットを出⼒
+      (0 to 8 + 2 - 2).foreach(i => bits(i) := bits(i + 1)) // 1ビット右シフト
+      //                  ↑ -2 なのは、                     ↑ ここで配列の要素数を越えないため
+      when(bitCounter === 0.U) { // ストップビットまで出⼒し終わったら
+        outValid := true.B
+        outBits := Cat(bits.slice(1, 8 + 2 - 1).reverse)
+        overrun := outValid // 前のデータが処理される前に次のデータの受信完了した
+        // 上のoutValid := true.Bと同時に入れられるので、普段はoverrunにはfalseが入るが
+        // やるべきタスクが終わっていない(outValid := false.Bがされていない)状態で来ると
+        // overrun が true になる
+        running := false.B
+      } .otherwise {
+        rateCounter := (baudDivider - 1).U
+        bitCounter := bitCounter - 1.U
+      }
+    } .otherwise {
+      rateCounter := rateCounter - 1.U
+    }
+  }
 }
