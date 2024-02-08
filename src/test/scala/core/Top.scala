0a4284a9495f6a5d1b9b3e5d1c5c6c7f29478b5c
wakuto <waku.902002@gmail.com>
2024-02-09 00:01:59 +0900

    vcdダンプを追加
    Co-authored-by: Kyuta <Ky00Ge@users.noreply.github.com>
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>

--- src/test/scala/core/Top.scala
+++ src/test/scala/core/Top.scala
@@ -2,11 +2,12 @@ package core
 
 import chisel3._
 import chiseltest._
+import chiseltest.simulator
 import chisel3.iotesters._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class TopTest extends AnyFlatSpec with ChiselScalatestTester {
-      it must "runs Top" in { test(new Core).withAnnotations(Seq(WriteFstAnnotation)) { c =>
+      it must "runs Top" in { test(new Core).withAnnotations(Seq(WriteFstAnnotation, WriteVcdAnnotation)) { c =>
         c.reset.poke(true.B)
         c.clock.step(4)
         c.reset.poke(false.B)
