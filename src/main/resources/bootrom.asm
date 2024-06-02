6916c3c5162bb23b31cf1b4688a8bb7baf5ca0fa
wakuto <waku.902002@gmail.com>
2024-06-02 22:55:02 +0900

    bootromのソースファイルを追加
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Kyuta <Ky00Ge@users.noreply.github.com>

---
+++ src/main/resources/bootrom.asm
@@ -0,0 +1,31 @@
+// Gpio:Init
+addi r1 = r0, 0
+out r0[4] = r1
+
+// Spi:Mode
+addi r1 = r0, 3
+out r0[2] = r1
+
+// Spi:Clockshamt
+addi r1 = r0, 4
+out r0[3] = r1
+
+// Loop:Setup
+addi r2 = r0, 97
+addi r3 = r0, 123
+
+// Spi:CS
+addi r1 = r0, 1
+out r0[4] = r1
+
+// Spi:Send
+out r0[1] = r2
+
+// Spi:CS
+addi r1 = r0, 0
+out r0[4] = r1
+
+// Loop
+addi r2 = r2, 1
+beq r0, (r2, r3) -> -48
+beq r0, (r0, r0) -> -42   
