8a7ef6a90dfe19840f9147be54e7d188dacb69f7
wakuto <waku.902002@gmail.com>
2024-06-15 22:39:41 +0900

    プログラムをラベル形式に書き換え
    Co-authored-by: Kyuta <Ky00Ge@users.noreply.github.com>
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>

--- src/main/resources/bootrom.asm
+++ src/main/resources/bootrom.asm
@@ -11,10 +11,12 @@ addi r1 = r0, 4
 out r0[3] = r1
 
 // Loop:Setup
+@setup
 addi r2 = r0, 97
 addi r3 = r0, 123
 
 // Spi:CS
+@loop
 addi r1 = r0, 1
 out r0[4] = r1
 
@@ -27,5 +29,5 @@ out r0[4] = r1
 
 // Loop
 addi r2 = r2, 1
-beq r0, (r2, r3) -> -48
-beq r0, (r0, r0) -> -42   
+beq r0, (r2, r3) -> setup
+beq r0, (r0, r0) -> loop
