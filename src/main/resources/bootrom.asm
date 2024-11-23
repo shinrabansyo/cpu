0fa4cdf8ca1c72dc658086102e9dbf08732fe2dd
wakuto <waku.902002@gmail.com>
2024-11-24 00:13:24 +0900

    gpio_write の検証プログラムを作成
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>
    Co-authored-by: Kota UCHIDA <uchan@uchan.net>

--- src/main/resources/bootrom.asm
+++ src/main/resources/bootrom.asm
@@ -1,98 +1,44 @@
-// byte1 0x41, 0x42, 0x43, 0x44, 0x45, 0x46
-// byte2 0x0102, 0x0304
-// byte4 0xdeadbeef
-// byte6 0xc0ffee
-
-// byte1 1, 2, 3, 4, 5
-// byte2 11, 12, 13, 14, 15
-// byte4 21, 22, 23, 24, 25
-// byte6 31, 32, 33, 34, 35
-// char 'g', 'h'
-// string "hello", "world"
-
-$str
-string "abc"
-
 ===
 
 // void main(void)
 @func_main
+    addi r10 = r0, 1
+    addi r11 = r0, 1
+    beq r1, (r0, r0) -> @func_gpio_write
 
-    @loop_func_main
+    @inf_loop.func_gpio_write
+    beq r0, (r0, r0) -> @inf_loop.func_gpio_write
 
-        // Call:init
-        // beq r1, (r0, r0) -> @func_get_freq
+// void  gpio_write(uint32_t pin, uint32_t value)
+@func_gpio_write
     
-        // 0xdeadbeefcafebabe - 0xbeefdeadbabecafe
-        addi r10 = r0, 0xcafebabe
-        addi r11 = r0, 0xdeadbeef
-        addi r12 = r0, 0xbabecafe
-        addi r13 = r0, 0xbeefdead
+    // シフト量 ピン番号-1 を計算
+    add r4 = r0, r10
+    subi r4 = r4, 1
 
-        // 0x0000000400000000 - 0x0000000000000001
-        addi r10 = r0, 0x00000000
-        addi r11 = r0, 0x00000004
-        addi r12 = r0, 0x00000001
-        addi r13 = r0, 0x00000000
- 
+    // 書き込む値を決定
+    beq r0, (r11, r0) -> @zero.func_gpio_write
+    addi r5 = r0, 1
+    beq r0, (r0, r0) -> @zero_end.func_gpio_write
+    @zero.func_gpio_write
+        addi r5 = r0, 0
+    @zero_end.func_gpio_write
+
+    // マスク準備
+    srl r4 = r5, r4
+    xori r5 = r4, 0xFFFFFFFF
+
+    // GPIOの現在状態を取得
+    in r6 = r0[4]
     
-        // Call:sub64
-        beq r1, (r0, r0) -> @func_sub64
+    // 0 にする: 元のレジスタの値 & 0xFFFFFFFE
+    // 1 にする: 元のレジスタの値 | 0x00000001
 
-        // Loop
-    @loop
-        add r0 = r10, r0
-        add r0 = r11, r0
-        beq r0, (r0, r0) -> @loop
+    // 現在状態にマスクをかける
+    and r7 = r5, r6
 
+    // GPIOの現在状態を更新
+    out r0[4] = r7
 
-// uint_32 get_count(void)
-@func_get_count
-
-    // In:Device(0x1000)
-    addi r4 = r0, 0x1000
-    in r5 = r4[0]
-
-    // Return
-    add r10 = r0, r5
+    // return
     jal r0, r1[0]
-
-
-// uint32 get_freq(void)
-@func_get_freq
-// In:Device(0x1002)
-    addi r4 = r0, 0x1002
-    in r5 = r4[0]
-
-    // Return
-    add r10 = r0, r5
-    jal r0, r1[0]
-
-// (uint32_t, uint32_t) sub64(uint64_t ah, uint64_t al, uint64_t bh, uint64_t bl)
-@func_sub64
-    // return a - b
-    // r10 = al, r11 = ah, r12 = bl, r13 = bh
-    
-    // 繰り下がりが発生したかを確認
-    // al < bl
-    blt r0, (r10, r12) -> @borrow.func_sub64
-
-    // 繰り下がりが発生しないならそのまま引き算
-    sub r10 = r10, r12
-    sub r11 = r11, r13
-    
-    // 早期リターン
-    jal r0, r1[0]
-
-    // 繰り下がりが発生
-    @borrow.func_sub64
-    // 下位はそのまま引き算
-    sub r10 = r10, r12
-    // 上位はそのまま引き算ののち -1
-    sub r11 = r11, r13
-    addi r4 = r0, 1
-    sub r11 = r11, r4
-
-    // return 
-    jal r0, r1[0]
-
